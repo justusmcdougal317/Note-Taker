9961f5496fdcccb83262f599e8b1b9cad3c0a721
justusmcdougal317 <142274754+justusmcdougal317@users.noreply.github.com>
2023-12-04 16:15:36 -0500

    updated changes

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up Handlebars as the view engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set the path for views (assuming your main.handlebars is in a 'views' folder)
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+
+const dataFilePath = path.join(__dirname, 'data.json');
+
+app.get('/', (req, res) => {
+  // Assuming main.handlebars is your main template file
+  res.render('main', { /* You can pass data to your template here if needed */ });
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile(dataFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  fs.readFile(dataFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    const notes = JSON.parse(data);
+
+    newNote.id = Date.now().toString();
+
+    notes.push(newNote);
+
+    fs.writeFile(dataFilePath, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;       
+      }
+
+      res.json(newNote);
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
